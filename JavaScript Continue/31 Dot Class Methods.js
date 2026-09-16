455995128c45f41d691905c33544549dfc4363c9
200~AI-and-Data-Science-4th <abdullahafridi296@gmail.com>
2026-09-16 22:04:31 +0500

    Class Methods using DOM

---
+++ JavaScript Continue/31 Dot Class Methods.js
@@ -0,0 +1,15 @@
+// .classlist to get the list of classes of an element
+document.querySelector(".box1").classList;
+
+// .className to get the class name of an element
+document.querySelector(".box1").className;
+
+// .classList.add() to add a class to an element
+document.querySelector(".box1").classList.add("new-class");
+
+// .classList.remove() to remove a class from an element
+document.querySelector(".box1").classList.remove("new-class");
+
+// .classList.toggle() to toggle a class on an element
+// if class is present it will be removed and if class in not present it will be added
+document.querySelector(".box1").classList.toggle("new-class");
