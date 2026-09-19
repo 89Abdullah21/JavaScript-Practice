717491b0f394cab6112a5dfab90faf50cd564715
200~AI-and-Data-Science-4th <abdullahafridi296@gmail.com>
2026-09-19 13:07:23 +0500

    Random Bg and text color generator for boxes

---
+++ JavaScript Continue/Exercises/Box Color Changer/script.js
@@ -0,0 +1,14 @@
+function changeColor() {
+    let r = Math.floor(0 + Math.random() * 255);
+    let g = Math.floor(0 + Math.random() * 255);
+    let b = Math.floor(0 + Math.random() * 255);
+    let color = `rgb(${r}, ${g}, ${b})`;
+    return color;
+}
+
+let boxes= document.querySelector(".boxes").children;
+
+Array.from(boxes).forEach(e =>{
+    e.style.backgroundColor = changeColor();
+    e.style.color = changeColor();
+})
